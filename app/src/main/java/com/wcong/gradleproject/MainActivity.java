630b31d3d7458042d6731d3e11c414b3e3c32692
wangcong <wangcong@hd123.com>
2018-08-10 14:27:23 +0800

    update

--- app/src/main/java/com/wcong/gradleproject/MainActivity.java
+++ app/src/main/java/com/wcong/gradleproject/MainActivity.java
@@ -1,7 +1,8 @@
 package com.wcong.gradleproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ((TextView) findViewById(R.id.txt)).setText(String.format("versionCode:\b%s\nversionName:\b%s", BuildConfig.VERSION_CODE, BuildConfig.VERSION_NAME));
     }
 }
